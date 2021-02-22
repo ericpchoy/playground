4537e3d9e53ee37017fe194d22e5c67eb76493a2
echoy1 <60661592+echoy1@users.noreply.github.com>
2021-02-21 21:36:40 -0800

    new stuff

--- src/Homework/Exercise3.js
+++ src/Homework/Exercise3.js
@@ -6,7 +6,17 @@ function Exercise3() {
     const [theme, setTheme] = useState('darktheme')
 
     return (
+        <div>
 
+        <div className='darktheme'>
+            This is dark theme
+        </div>
+
+        <div>
+        <button onClick={()=>setTheme('lighttheme')}>Change to Light Theme</button>
+        </div>
+        
+        </div>
     )
 }
 
